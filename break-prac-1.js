da35275b78a246ce68b3c2607e35833501ce2885
Hasanfunn <mamunhosein99@gmail.com>
2025-07-31 11:02:08 +0600

    Jus started With the Loop Break Practice

---
+++ break-prac-1.js
@@ -0,0 +1,16 @@
+// let num = 0;
+// while (num <200){
+//     num=num+1;
+//     if(num >=100){
+//         break;
+//     }
+//     console.log(num);
+// }
+
+for(i=0; i<200; i++){
+    if(i ==120){
+        break
+    }
+    console.log(i);
+    
+}
